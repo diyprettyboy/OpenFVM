55e9fbc5b367e18d2dd7b95e6b8c2d0c38417b8d
diyprettyboy <xuxiang.cao@hotmail.com>
2022-12-20 17:06:24 +0800

    change

--- serial/source/variables.h
+++ serial/source/variables.h
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "../laspack/laspack.h"
+#include "laspack.h"
 
 Vector Co;
 Vector uf;
